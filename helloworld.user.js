ec332d2214a109d618ae63ea5fbc7166e505f0e6
Sebastian Naicker <djinnwheeler@gmail.com>
2013-06-15 13:58:45 +0300

    alert dialog box added to helloworld.user.js

---
+++ helloworld.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @author  	  Sebastian Naicker
+// @name          Hello World
+// @discription   This script is for the purpose of testing.
+//                It displays an alert box with the string 'HELLO WORLD !' in it.
+// @require       http://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js
+
+/*/--This include is just for temporary testing purposes*/
+// @include		  *
+/*--/*/
+
+// @version       0.1
+// @grant		  none
+// ==/UserScript==
+
+
+/*This will display a pop-up with 'HELLO WORLD !'*/
+alert('HELLO WORLD !');
